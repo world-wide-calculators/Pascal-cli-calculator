249bf1276621edb5236fab0f14ae41f9f3f17b13
BM835 <devdev@bm835.space>
2019-08-31 10:57:18 +1000

    Retry added

--- calc.pas
+++ calc.pas
@@ -2,26 +2,29 @@ program calc;
 uses crt;
 var
   n1, n2, out : Integer;
-  op: Char;
+  op, retry: Char;
 begin
-  Write('Input first number: ');
-  ReadLn(n1);
-  Write('Input operator: ');
-  ReadLn(op);
-  Write('Input second number: ');
-  ReadLn(n2);
-  if op = '+' then
-    WriteLn(n1, op, n2, '=', n1 + n2)
-  else
-    if op = '-' then
-      WriteLn(n1, op, n2, '=', n1 - n2)
+  repeat
+    Write('Input first number: ');
+    ReadLn(n1);
+    Write('Input operator: ');
+    ReadLn(op);
+    Write('Input second number: ');
+    ReadLn(n2);
+    if op = '+' then
+      WriteLn(n1, op, n2, '=', n1 + n2)
     else
-      if op = '*' then
-        WriteLn(n1, op, n2, '=', n1 * n2)
+      if op = '-' then
+        WriteLn(n1, op, n2, '=', n1 - n2)
       else
-        if op = '/' then
-          WriteLn(n1, op, n2, '=', n1 / n2)
+        if op = '*' then
+          WriteLn(n1, op, n2, '=', n1 * n2)
         else
-          WriteLn('Sorry, wrong operator');
-  readln;
+          if op = '/' then
+            WriteLn(n1, op, n2, '=', n1 / n2)
+          else
+            WriteLn('Sorry, wrong operator');
+    Write('Retry? (y/n): ');
+    ReadLn(retry);
+  until retry <> 'y';
 end.
